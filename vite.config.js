7cdf67387ac56d9c90e3769a42664088bd258915
MOIN MALIK <moinmalik2589@gmail.com>
2026-08-20 14:14:45 +0530

    Final App ok

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/Velocity-Legends/",
+    base: "/velocity-legends/",
+
     server: {
         port: 1807,
         strictPort: true
     },
+
     build: {
         target: "es2020",
-        sourcemap: false,
-        rolldownOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes("node_modules/three/")) return "three";
-                    if (id.includes("node_modules/phaser/")) return "phaser";
-                }
-            }
-        }
+        sourcemap: false
     }
 });
